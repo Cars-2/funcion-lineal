c928e8d04abb3bda8b8fd20e8d0fe889af05e4c0
caritom <146208468+Cars-2@users.noreply.github.com>
2023-09-29 08:51:32 -0300

    Update helloworld/ explloto todom.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,9 @@
+m = float(input("Ingrese el valor de m: "))
+b = float(input("Ingrese el valor de b: "))
+# Definir la función lineal y = mx + b
+def funcion_lineal(m, b):
+    return -b / m
+    
+print(f'La raíz de la función lineal es x = {funcion_lineal(m, b)}')
 print("Hello world")
 print("explode the world")
